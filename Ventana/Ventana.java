980d1a4db0b83c30e35066aeeaa10ce713e5a577
IgnCan <igcandia2017@udec.cl>
2023-11-02 03:17:22 -0300

    Update Ventana.java

--- Ventana/Ventana.java
+++ Ventana/Ventana.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class Ventana extends JFrame {
     public Ventana(){
 //        Esto indica el ancho y largo respectivamente, recibe dos int
-        this.setSize(500,500);
+        this.setSize(1000,800);
 
 //         Establece la posicion donde aparece la ventana
 //        setLocation(250,250);
@@ -23,14 +23,39 @@ public class Ventana extends JFrame {
         // Se explica solo
         setTitle("Bonito Dispensador :D (estoy cansado jefe)");
 
-        Paneles();
+        paneles();
+
+
     }
 
-    private void Paneles(){
+    private void paneles(){
         //Crea el panel
+        setLayout(new BorderLayout());
+
+        JPanel monedas = new JPanel();
+        monedas.setBackground(Color.RED);
+        this.getContentPane().add(monedas);
+
+
         JPanel dispensador = new JPanel();
         dispensador.setBackground(Color.CYAN);
         this.getContentPane().add(dispensador);
+
+
+        JPanel comprador = new JPanel();
+        comprador.setBackground(Color.BLACK);
+        this.getContentPane().add(comprador);
+
+
+        add(monedas,BorderLayout.WEST);
+        add(dispensador,BorderLayout.CENTER);
+        add(comprador,BorderLayout.EAST);
+
+        monedas.setPreferredSize(new Dimension(300, 400));
+        dispensador.setPreferredSize(new Dimension(400, 400));
+        comprador.setPreferredSize(new Dimension(300, 400));
+
+
     }
 
 }
