6aaf91901c6e88e6fdcef74c7f8d1a7f76768e54
IgnCan <igcandia2017@udec.cl>
2023-11-08 19:56:25 -0300

    Cambiosmayores

--- Main.java
+++ Main.java
@@ -1,4 +1,8 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
+
     public static void main(String[] args){
         Ventana v1 = new Ventana();
         v1.setVisible(true);
--- PanelDispensador.java
+++ PanelDispensador.java
@@ -7,88 +7,45 @@ public class PanelDispensador extends JPanel {
     public PanelDispensador(){
         super();
         setBackground(Color.PINK);
-        JPanel p1=new JPanel();
 
         JLabel label= new JLabel("LabelCoca");
-
         JButton botoncoca = new JButton("Cocacola");
-        //p1.add(botoncoca);
-        botoncoca.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Acción a realizar cuando se hace clic en el botón
-//                label.setText("");
-                System.out.println("CocaCola");
-//                repaint();
-            }
-        });
-
         JLabel label2= new JLabel("LabelSprite");
-
         JButton botonsprite = new JButton("Sprite");
-        //p1.add(botonsprite);
-        botonsprite.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Acción a realizar cuando se hace clic en el botón
-                JLabel label2= new JLabel("LabelCoca");
-                System.out.println("Sprite");
-                repaint();
-            }
-        });
-
         JLabel label3= new JLabel("LabelFanta");
-
         JButton botonfanta = new JButton("Fanta");
-        //p1.add(botonfanta);
-        botonfanta.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Acción a realizar cuando se hace clic en el botón
-                JLabel label3= new JLabel("LabelCoca");
-                System.out.println("Fanta");
-                repaint();
-            }
-        });
-
         JLabel label4= new JLabel("LabelSpuer8");
-
         JButton botonsuper8 = new JButton("Super8");
-        //p1.add(botonsuper8);
-        botonsuper8.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Acción a realizar cuando se hace clic en el botón
-                JLabel label4= new JLabel("LabelCoca");
-                System.out.println("Super8");
-                repaint();
-            }
-        });
-
-         JLabel label5= new JLabel("LabelSnikers");
-
+        JLabel label5= new JLabel("LabelSnikers");
         JButton botonsnikers = new JButton("Snikers");
-        p1.add(botonsnikers);
-        botonsnikers.addActionListener(new ActionListener() {
-            @Override
+        JLabel labelCompra = new JLabel("Aca sale la compra");
+        JLabel vuelto = new JLabel("De aca saldra el vuelto");
+
+
+
+        botoncoca.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                // Acción a realizar cuando se hace clic en el botón
-                JLabel label5= new JLabel("LabelCoca");
-                System.out.println("Snikers");
-                repaint();
+                System.out.println("CocaCola");
             }
         });
-
-
-
-        JButton Comprar= new JButton("Comprar");
-        add(Comprar);
-        botonsnikers.addActionListener(new ActionListener() {
-            @Override
+        botonsprite.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                // Acción a realizar cuando se hace clic en el botón
-                System.out.println("Compra realizada");
-                repaint();
+                System.out.println("Sprite");
+            }
+        });
+        botonfanta.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Fanta");
+            }
+        });
+        botonsuper8.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Super8");
+            }
+        });
+        botonsnikers.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Snikers");
             }
         });
 
@@ -119,18 +76,63 @@ public class PanelDispensador extends JPanel {
         add(botonfanta,gbc);
         botonfanta.setPreferredSize(buttonSize);
 
-        gbc.gridy = 4; // Fila 3
+
+        gbc.gridx = 1; // Columna 1
+
+        gbc.gridy = 1; // Fila 1
+        add(label,gbc);
+        label.setPreferredSize(buttonSize);
+
+        gbc.gridy = 2; // Fila 2
+        add(label2,gbc);
+        label2.setPreferredSize(buttonSize);
+
+        gbc.gridy = 3; // Fila 3
+        add(label3,gbc);
+        label3.setPreferredSize(buttonSize);
+
+
+
+        gbc.gridx = 3; // Columna 3
+
+        gbc.gridy = 1; // Fila 1
         add(botonsuper8,gbc);
         botonsuper8.setPreferredSize(buttonSize);
 
-        gbc.gridy = 5; // Fila 4
+        gbc.gridy = 2; // Fila 2
         add(botonsnikers,gbc);
         botonsnikers.setPreferredSize(buttonSize);
 
-        gbc.gridx = 1; // Columna 1
-        gbc.gridy = 3; // Fila 4
-        add(Comprar,gbc);
-        Comprar.setPreferredSize(buttonSize);
+        gbc.gridy = 4; // Fila 2
+        JLabel aa=new JLabel("   ");
+        add(aa,gbc);
+        aa.setPreferredSize(buttonSize);
+
+        gbc.gridy = 7; // Fila 5
+        add(labelCompra,gbc);
+        labelCompra.setPreferredSize(buttonSize);
+
+
+
+
+        gbc.gridx = 4; // Columna 4
+
+        gbc.gridy = 1; // Fila 1
+        add(label4,gbc);
+        label4.setPreferredSize(buttonSize);
+
+        gbc.gridy = 2; // Fila 2
+        add(label5,gbc);
+        label5.setPreferredSize(buttonSize);
+
+        gbc.gridy = 4; // Fila 2
+        add(aa,gbc);
+        aa.setPreferredSize(buttonSize);
+
+
+        gbc.gridy = 7; // Fila 5
+        add(vuelto,gbc);
+        vuelto.setPreferredSize(buttonSize);
 
     }
 }
