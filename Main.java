61e1145788a0198a35c1b80dc4218cf2da308049
paulavsmt <pasanmartin2021@udec.cl>
2023-11-09 09:17:32 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException {
         Ventana v1 = new Ventana();
-        v1.setVisible(true);
+        //v1.setVisible(true);
     }
 }
