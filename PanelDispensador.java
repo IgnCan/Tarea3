b4cadaff6f6caa71b75a6511a2c0ceddb142dcbf
paulavsmt <pasanmartin2021@udec.cl>
2023-11-08 22:23:36 -0300

    Update PanelDispensador.java

--- PanelDispensador.java
+++ PanelDispensador.java
@@ -4,6 +4,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class PanelDispensador extends JPanel {
+
+    Expendedor exp = new Expendedor(1);
+    Moneda m = null;
+    Comprador c = null;
+
+
     public PanelDispensador(){
         super();
         setBackground(Color.PINK);
