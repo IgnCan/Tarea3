3903f300c93c1408a66e1fa96b3fdb0f5557c344
paulavsmt <pasanmartin2021@udec.cl>
2023-11-11 03:00:56 -0300

    Update PanelConImagenes.java

--- PanelConImagenes.java
+++ PanelConImagenes.java
@@ -1,27 +1,69 @@
 import javax.swing.*;
+import java.awt.*;
 
-public class PanelConImagenes extends JPanel {
+public class PanelConImagenes extends JLabel {
+    JLabel Etiqueta;
+    public PanelConImagenes(int i) {
 
-    public PanelConImagenes() {
-
-
-        // Cargar imágenes desde archivos (ajusta las rutas según sea necesario)
-        ImageIcon imagen1 = new ImageIcon("\"C:\\Users\\snmar\\Downloads\\cocacola.jpg\"");
-        ImageIcon imagen2 = new ImageIcon("\"C:\\Users\\snmar\\Downloads\\Sprite.jpg\"");
-        ImageIcon imagen3 = new ImageIcon("\"C:\\Users\\snmar\\Downloads\\fanta.jpg\"");
-        ImageIcon imagen4 = new ImageIcon("\"C:\\Users\\snmar\\Downloads\\super8.jpg\"");
-        ImageIcon imagen5 = new ImageIcon("\"C:\\Users\\snmar\\Downloads\\snikers.jpg\"");
+        switch (i){
+            case 1:{
+            ImageIcon imagen1 = new ImageIcon("\"C:\\Users\\snmar\\Downloads\\cocacola.jpg\"");
+            Etiqueta = new JLabel(imagen1);
 
 
-        // Crear etiquetasmostrar las imágenes
-        JLabel etiqueta1 = new JLabel(imagen1);
-        JLabel etiqueta2 = new JLabel(imagen2);
-        JLabel etiqueta3 = new JLabel(imagen1);
-        JLabel etiqueta4 = new JLabel(imagen2);
-        JLabel etiqueta5 = new JLabel(imagen2);
+            }
+            case 2:{
+                ImageIcon imagen2 = new ImageIcon("\"C:\\Users\\snmar\\Downloads\\Sprite.jpg\"");
+                Etiqueta = new JLabel(imagen2);
 
-        // Agregar las etiquetas al panel
-        add(etiqueta1);
-        add(etiqueta2);
+            }
+            case 3:{
+                ImageIcon imagen3 = new ImageIcon("\"C:\\Users\\snmar\\Downloads\\fanta.jpg\"");
+                Etiqueta= new JLabel(imagen3);
+
+            }
+            case 4:{
+                ImageIcon imagen4 = new ImageIcon("\"C:\\Users\\snmar\\Downloads\\super8.jpg\"");
+                Etiqueta= new JLabel(imagen4);
+
+            }
+            case 5:{
+                ImageIcon imagen5 = new ImageIcon("\"C:\\Users\\snmar\\Downloads\\snikers.jpg\"");
+                Etiqueta = new JLabel(imagen5);
+
+
+        }
+            Dimension LABELZISE = new Dimension(100, 100);
+            Etiqueta.setPreferredSize(LABELZISE);
+
+      }
     }
 }
+
+//public class JLabelConImagen extends JFrame {
+//    public JLabelConImagen() {
+//        // Cargar la imagen desde un archivo (asegúrate de tener la ruta correcta)
+//        ImageIcon icono = new ImageIcon("ruta/de/la/imagen.jpg");
+//
+//        // Escalar la imagen si es necesario
+//        Image imagen = icono.getImage();
+//        Image nuevaImagen = imagen.getScaledInstance(200, 200, java.awt.Image.SCALE_SMOOTH);
+//        icono = new ImageIcon(nuevaImagen);
+//
+//        // Crear el JLabel y establecer el ImageIcon
+//        JLabel label = new JLabel(icono);
+//
+//        // Agregar el JLabel al JFrame
+//        add(label);
+//
+//        // Configurar el JFrame
+//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        setSize(300, 300);
+//        setLocationRelativeTo(null);
+//        setVisible(true);
+//    }
+//
+//    public static void main(String[] args) {
+//        new JLabelConImagen();
+//    }
+//}
