0fa201148dc2e294deb9c92d3b9a778a9f41974f
paulavsmt <pasanmartin2021@udec.cl>
2023-11-12 20:03:25 -0300

    Update Ventana.java

--- Ventana.java
+++ Ventana.java
@@ -26,7 +26,7 @@ public class Ventana extends JFrame {
         // Cierra el programa al cerrar la ventana
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        setTitle("Bonito Dispensador :D (estoy cansado jefe)");
+        setTitle("Panel Dispensador");
 
         //Crea el panel
         setLayout(new BorderLayout());
